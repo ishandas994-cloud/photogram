73860ea7e3261cfbcb63fba2928243fe5e1832b4
ishandas994-cloud <ishandas994@gmail.com>
2026-05-12 13:21:57 +0530

    new code

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ const searchRoutes = require('./routes/search');
 // =========================
 const app = express();
 
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
 const server = http.createServer(app);
 
@@ -297,3 +297,4 @@ if (process.env.NODE_ENV !== 'production') {
 // Export for Vercel
 // =========================
 module.exports = app;
+app.set('trust proxy', 1);
