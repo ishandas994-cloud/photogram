f7f061f57b359d23ae816157d0b4254fb0a143c3
ishandas994-cloud <ishandas994@gmail.com>
2026-05-12 12:08:32 +0530

    new code

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,11 @@
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host:     process.env.DB_HOST     || 'localhost',
-  port:     parseInt(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME     || 'photogram',
-  user:     process.env.DB_USER     || 'postgres',
-  password: process.env.DB_PASSWORD || '23032005',
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionString: process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on('error', (err) => {
@@ -18,9 +14,15 @@ pool.on('error', (err) => {
 });
 
 const db = async (strings, ...values) => {
-  const text = strings.reduce((acc, str, i) =>
-    acc + str + (i < values.length ? '$' + (i+1) : ''), '');
+
+  const text = strings.reduce(
+    (acc, str, i) =>
+      acc + str + (i < values.length ? '$' + (i + 1) : ''),
+    ''
+  );
+
   const client = await pool.connect();
+
   try {
     return await client.query(text, values);
   } finally {
@@ -28,8 +30,8 @@ const db = async (strings, ...values) => {
   }
 };
 
-db.pool    = pool;
-db.query   = (text, params) => pool.query(text, params);
+db.pool = pool;
+db.query = (text, params) => pool.query(text, params);
 db.connect = () => pool.connect();
 
 module.exports = db;
